c4490bae942abb12f7ce891ebaf97dddc9da7a78
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-05-25 16:36:05 +0300

    header markup, CSS header, JS menu, IMG for header + sprite for all

---
+++ src/main.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-button]'),
+    closeMenuBtn: document.querySelector('[data-menu-button-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.menu.classList.toggle('is-open');
+    refs.openMenuBtn.classList.toggle('is-open');
+  }
+})();
