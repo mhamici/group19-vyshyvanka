b1d8c39f5fe3ab96699980bc19b60cae289d7fe5
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-05-26 18:13:34 +0300

    add no scroll + background mob menu

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector('[data-menu-button]'),
     closeMenuBtn: document.querySelector('[data-menu-button-close]'),
     menu: document.querySelector('[data-menu]'),
+    overlay: document.querySelector('.overlay'), // добавляем ссылку на элемент overlay
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
@@ -11,5 +12,8 @@
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
     refs.openMenuBtn.classList.toggle('is-open');
+    refs.overlay.classList.toggle('is-open'); // добавляем/удаляем класс для overlay
+    
+    document.body.classList.toggle('menu-open');
   }
 })();
